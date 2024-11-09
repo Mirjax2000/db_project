cdce04f60cdba54a3a28a635cd58237886715dec
Vigo <miroslav.viktorin@seznam.cz>
2024-11-09 12:48:20 +0100

    update students

--- DDL/Tables/course.sql
+++ DDL/Tables/course.sql
@@ -1,14 +1,8 @@
--- DROP TABLE IF EXISTS course;
--- CREATE TABLE course (
---     course_id SERIAL PRIMARY KEY,
---     course_name VARCHAR(50) NOT NULL,
---     description VARCHAR(200) DEFAULT NULL,
---     trainer_id SMALLINT,
---     FOREIGN KEY (trainer_id) REFERENCES trainer(trainer_id)
--- );
-INSERT INTO
-    course (course_name, description, trainer_id)
-VALUES
-    ('Python basic', 'zaklady Pythonu', 1),
-    ('HTML, CSS', 'Front End', 2),
-    ('GIT', 'verzovaci system GIT', 3);
+DROP TABLE IF EXISTS course;
+CREATE TABLE course (
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(50) NOT NULL,
+    description VARCHAR(200) DEFAULT NULL,
+    trainer_id SMALLINT,
+    FOREIGN KEY (trainer_id) REFERENCES trainer(trainer_id)
+);
--- DDL/data.loads.sql
+++ DDL/data.loads.sql
@@ -1,17 +1,82 @@
+-- INSERT INTO
+--     student (first_name, last_name, email, phone)
+-- VALUES
+--     (
+--         'David',
+--         'Zelenac',
+--         'david.zelenac@gamil.com',
+--         '634441808'
+--     ),
+--     (
+--         'Josef',
+--         'Hracicka',
+--         'josef.hracicka@gmail.com',
+--         '422984837'
+--     ),
+--     (
+--         'Karel',
+--         'Pohadkar',
+--         'karel.pohadkar@gmail.com',
+--         '604250242'
+--     ),
+--     (
+--         'Filip',
+--         'Buchta',
+--         'filip.buchta@gmail.com',
+--         '871788018'
+--     );
+-- INSERT INTO
+--     trainer (first_name, last_name, email, phone, expertise)
+-- VALUES
+--     (
+--         'Adam',
+--         'Bublina',
+--         'adam.bublina@sdacdmy.com',
+--         '451680165',
+--         'basic python'
+--     ),
+--     (
+--         'Petr',
+--         'Cmoudik',
+--         'petr.cmoudik@sdacdemy.com',
+--         '391630343',
+--         'HTML,CSS'
+--     ),
+--     (
+--         'Jan',
+--         'Duchacek',
+--         'jan.duchacek@sdacdemy.com',
+--         '126184158',
+--         'GIT'
+--     );
+-- INSERT INTO
+--     course (course_name, description, trainer_id)
+-- VALUES
+--     ('Python basic', 'zaklady Pythonu', 1),
+--     ('HTML, CSS', 'Front End', 2),
+--     ('GIT', 'verzovaci system GIT', 3);
+-- INSERT INTO
+--     student_course_signup (course_id, student_id)
+-- VALUES
+--     (1, 1),
+--     (2, 1),
+--     (2, 2),
+--     (3, 2);
+#
 INSERT INTO
     student (first_name, last_name, email, phone)
 VALUES
     (
-        'David',
-        'Zelenac',
-        'david.zelenac@gamil.com',
-        '634441808'
+        'Filip',
+        'Pohadkomil',
+        'filip.ohadkomil@seznam.cz',
+        '813653694'
     ),
     (
-        'Josef',
-        'Hracicka',
-        'josef.hracicka@gmail.com',
-        '422984837'
+        'Jaroslav',
+        'Zmrzlina',
+        'jaroslav.zmrzlina@gmail.com',
+        '755536216'
     ),
     (
         'Karel',
@@ -20,48 +85,20 @@ VALUES
         '604250242'
     ),
     (
-        'Filip',
-        'Buchta',
-        'filip.buchta@gmail.com',
-        '871788018'
-    );
-
-INSERT INTO
-    trainer (first_name, last_name, email, phone, expertise)
-VALUES
-    (
-        'Adam',
-        'Bublina',
-        'adam.bublina@sdacdmy.com',
-        '451680165',
-        'basic python'
+        'Jakub',
+        'Zazrak',
+        'jakub.Zazrak@gmail.com',
+        '987892868'
     ),
     (
-        'Petr',
-        'Cmoudik',
-        'petr.cmoudik@sdacdemy.com',
-        '391630343',
-        'HTML,CSS'
+        'Arnost',
+        'Mrkvicka',
+        'arnost.mrkvicka@mrkev.cz',
+        '127845332'
     ),
     (
-        'Jan',
-        'Duchacek',
-        'jan.duchacek@sdacdemy.com',
-        '126184158',
-        'GIT'
+        'Karel',
+        'Vopicka',
+        'karel.vopicka.com',
+        '987889041'
     );
-
-INSERT INTO
-    course (course_name, description, trainer_id)
-VALUES
-    ('Python basic', 'zaklady Pythonu', 1),
-    ('HTML, CSS', 'Front End', 2),
-    ('GIT', 'verzovaci system GIT', 3);
-
-INSERT INTO
-    student_course_signup (course_id, student_id)
-VALUES
-    (1, 1),
-    (2, 1),
-    (2, 2),
-    (3, 2);
