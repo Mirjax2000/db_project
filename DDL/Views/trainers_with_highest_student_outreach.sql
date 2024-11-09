6a9b8958f389de32114a2e6eddba65e7a2ea2a91
Vigo <miroslav.viktorin@seznam.cz>
2024-11-09 13:45:51 +0100

    update

--- DDL/Views/trainers_with_highest_student_outreach.sql
+++ DDL/Views/trainers_with_highest_student_outreach.sql
@@ -2,4 +2,8 @@
 -- jinymi slovy trenery, kteri vyucuji kurzy s nejvetsim poctem zapsanych studentu
 
 CREATE VIEW full AS
+SELECT course_id, COUNT(student_id) FROM student_course_signup
+GROUP BY course_id ;
+
+-- tady to musim napojit na course tabulku na id_trainer
 
