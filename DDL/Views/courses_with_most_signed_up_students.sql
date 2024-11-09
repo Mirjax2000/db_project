7ffb24b5cb40da89504af6588bd119d2617f77aa
Vigo <miroslav.viktorin@seznam.cz>
2024-11-09 13:26:36 +0100

    prvni views

--- DDL/Views/courses_with_most_signed_up_students.sql
+++ DDL/Views/courses_with_most_signed_up_students.sql
@@ -1,1 +1,7 @@
 -- tento view vraci TOP 5 kurzu s nejvice zapsanymi studenty
+CREATE VIEW top_5 AS
+SELECT course_id, COUNT(student_id)
+FROM student_course_signup
+GROUP BY course_id
+ORDER BY count DESC
+LIMIT 5;
