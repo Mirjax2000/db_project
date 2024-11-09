a5ed689afe174c7f0f394ac485fb043e53859fe4
Vigo <miroslav.viktorin@seznam.cz>
2024-11-09 11:29:46 +0100

    trainer done

--- DDL/Tables/trainer.sql
+++ DDL/Tables/trainer.sql
@@ -1,11 +1,34 @@
-DROP TABLE IF EXISTS trainer;
+-- CREATE TABLE
+--     trainer (
+--         trainer_id SERIAL PRIMARY KEY,
+--         first_name VARCHAR(50) NOT NULL,
+--         last_name VARCHAR(200) NOT NULL,
+--         email VARCHAR(100) NOT NULL,
+--         phone VARCHAR(10) NOT NULL,
+--         expertise VARCHAR(20)
+--     );
 
-CREATE TABLE
-    trainer (
-        trainer_id SERIAL PRIMARY KEY,
-        first_name VARCHAR(50) NOT NULL,
-        last_name VARCHAR(200) NOT NULL,
-        email VARCHAR(100) NOT NULL,
-        phone VARCHAR(10) NOT NULL,
-        expertise VARCHAR(20)
+INSERT INTO
+    trainer (first_name, last_name, email, phone, expertise)
+VALUES
+    (
+        'Adam',
+        'Bublina',
+        'adam.bublina@sdacdmy.com',
+        '451680165',
+        'basic python'
+    ),
+    (
+        'Petr',
+        'Cmoudik',
+        'petr.cmoudik@sdacdemy.com',
+        '391630343',
+        'HTML,CSS'
+    ),
+    (
+        'Jan',
+        'Duchacek',
+        'jan.duchacek@sdacdemy.com',
+        '126184158',
+        'GIT'
     );
