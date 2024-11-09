65a3284c7bdb746d85c357caa726a23d6c67a6cf
Vigo <miroslav.viktorin@seznam.cz>
2024-11-09 13:38:14 +0100

    wiev update

--- DDL/Views/trainers_teaching_atleast_2_courses.sql
+++ DDL/Views/trainers_teaching_atleast_2_courses.sql
@@ -1,1 +1,12 @@
 -- tento view vraci trenery kteri vyucuji alespon 2 kurzy
+CREATE VIEW
+    skilled AS
+SELECT
+    trainer_id,
+    COUNT(trainer_id)
+FROM
+    course
+GROUP BY
+    trainer_id
+HAVING
+    COUNT(trainer_id) >= 2;
