4c8a276dc03f887c0f3e39746c757144dbbd7e5b
Vigo <miroslav.viktorin@seznam.cz>
2024-11-09 11:03:11 +0100

    table course

--- DDL/Tables/course.sql
+++ DDL/Tables/course.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS course;
 
 CREATE TABLE course (
-    id SERIAL PRIMARY KEY NOT NULL,
-    course_name VARCHAR(50) NOT NULL
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(50) NOT NULL,
+    description VARCHAR(200) DEFAULT NULL,
+    trainer_id SMALLINT,
+    FOREIGN KEY (trainer_id) REFERENCES trainer(trainer_id)
 );
