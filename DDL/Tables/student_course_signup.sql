597661aaab9e4487d41f7018b3e7c71f355d79e9
Vigo <miroslav.viktorin@seznam.cz>
2024-11-10 15:29:18 +0100

    ach jo

--- DDL/Tables/student_course_signup.sql
+++ DDL/Tables/student_course_signup.sql
@@ -6,3 +6,36 @@ CREATE TABLE
         course_id SMALLINT NOT NULL REFERENCES course (course_id),
         student_id SMALLINT NOT NULL REFERENCES student (student_id)
     );
+
+INSERT INTO
+    student_course_signup (course_id, student_id)
+VALUES
+    (6, 3),
+    (1, 1),
+    (1, 2),
+    (1, 4),
+    (1, 6),
+    (1, 7),
+    (1, 8),
+    (1, 9),
+    (2, 1),
+    (2, 3),
+    (2, 10),
+    (3, 1),
+    (3, 2),
+    (3, 3),
+    (3, 4),
+    (3, 5),
+    (3, 6),
+    (3, 7),
+    (3, 8),
+    (3, 8),
+    (3, 9),
+    (3, 10),
+    (4, 3),
+    (4, 1),
+    (4, 7),
+    (4, 7),
+    (5, 3),
+    (5, 10),
+    (5, 6);
--- DDL/data.loads.sql
+++ DDL/data.loads.sql
@@ -1,12 +1,21 @@
 INSERT INTO
+    address (street, city, zip_code)
+VALUES
+    ('Main St 123', 'Springfield', '12345'),
+    ('Oak Ave 456', 'Rivertown', '67890'),
+    ('Maple Rd 789', 'Lakeside', '11223'),
+    ('Sunset Blvd 101', 'Hilltop', '33445'),
+    ('Elm St 202', 'Greenfield', '55667'),
+    ('River Rd 303', 'Brookville', '77889'),
+    ('Park Ave 404', 'Clearwater', '99001'),
+    ('Cedar Ln 505', 'Woodland', '22334'),
+    ('Pine St 606', 'Evergreen', '44556'),
+    ('Birch Rd 707', 'Rockville', '66778');
+
+INSERT INTO
     student (first_name, last_name, email, phone)
 VALUES
-    (
-        'David',
-        'Zelenac',
-        'david.zelenac@gamil.com',
-        '634441808'
-    ),
+    ('David', 'Zelenac', 'david.zelenac@gamil.com', '634441808'),
     (
         'Josef',
         'Hracicka',
@@ -19,12 +28,7 @@ VALUES
         'karel.pohadkar@gmail.com',
         '604250242'
     ),
-    (
-        'Filip',
-        'Buchta',
-        'filip.buchta@gmail.com',
-        '871788018'
-    ),
+    ('Filip', 'Buchta', 'filip.buchta@gmail.com', '871788018'),
     (
         'Filip',
         'Pohadkomil',
@@ -43,24 +47,14 @@ VALUES
         'karel.pohadkar@gmail.com',
         '604250242'
     ),
-    (
-        'Jakub',
-        'Zazrak',
-        'jakub.Zazrak@gmail.com',
-        '987892868'
-    ),
+    ('Jakub', 'Zazrak', 'jakub.Zazrak@gmail.com', '987892868'),
     (
         'Arnost',
         'Mrkvicka',
         'arnost.mrkvicka@mrkev.cz',
         '127845332'
     ),
-    (
-        'Karel',
-        'Vopicka',
-        'karel.vopicka.com',
-        '987889041'
-    );
+    ('Karel', 'Vopicka', 'karel.vopicka.com', '987889041');
 
 INSERT INTO
     trainer (first_name, last_name, email, phone, expertise)
@@ -110,50 +104,3 @@ VALUES
     ('GIT', 'verzovaci system GIT', 3),
     ('SQL', 'databaze', 4),
     ('Python advanced', 'Pokrocily Python', 5);
-
-INSERT INTO
-    student_course_signup (course_id, student_id)
-VALUES
-    (6, 3),
-    (1, 1),
-    (1, 2),
-    (1, 4),
-    (1, 6),
-    (1, 7),
-    (1, 8),
-    (1, 9),
-    (2, 1),
-    (2, 3),
-    (2, 10),
-    (3, 1),
-    (3, 2),
-    (3, 3),
-    (3, 4),
-    (3, 5),
-    (3, 6),
-    (3, 7),
-    (3, 8),
-    (3, 8),
-    (3, 9),
-    (3, 10),
-    (4, 3),
-    (4, 1),
-    (4, 7),
-    (4, 7),
-    (5, 3),
-    (5, 10),
-    (5, 6);
-
--- INSERT INTO
---     address (street, city, zip_code)
--- VALUES
---     ('Main St 123', 'Springfield', '12345'),
---     ('Oak Ave 456', 'Rivertown', '67890'),
---     ('Maple Rd 789', 'Lakeside', '11223'),
---     ('Sunset Blvd 101', 'Hilltop', '33445'),
---     ('Elm St 202', 'Greenfield', '55667'),
---     ('River Rd 303', 'Brookville', '77889'),
---     ('Park Ave 404', 'Clearwater', '99001'),
---     ('Cedar Ln 505', 'Woodland', '22334'),
---     ('Pine St 606', 'Evergreen', '44556'),
---     ('Birch Rd 707', 'Rockville', '66778');
