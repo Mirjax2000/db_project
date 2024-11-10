37c419ace3723d4abc8b0f1ca17995ed34bca8a6
Vigo <miroslav.viktorin@seznam.cz>
2024-11-10 17:44:55 +0100

    update

--- DDL/Views/trainers_teaching_atleast_2_courses.sql
+++ DDL/Views/trainers_teaching_atleast_2_courses.sql
@@ -6,14 +6,14 @@ DROP VIEW IF EXISTS skilled;
 CREATE VIEW
     skilled AS
 SELECT
-    trainer.first_name,
-    trainer.last_name,
-    COUNT(course.trainer_id)
+    trainer.first_name AS Jmeno,
+    trainer.last_name AS Prijmeni,
+    COUNT(course.course_id) AS pocet_kurzu
 FROM
-    course
-    INNER JOIN trainer ON trainer.trainer_id = course.trainer_id
+    trainer
+    INNER JOIN course ON trainer.trainer_id = course.trainer_id
 GROUP BY
     trainer.first_name,
     trainer.last_name
 HAVING
-    COUNT(course.trainer_id) >= 2;
+    COUNT(course.course_id) >= 2;
--- DDL/Views/trainers_with_highest_student_outreach.sql
+++ DDL/Views/trainers_with_highest_student_outreach.sql
@@ -1,9 +1,17 @@
 -- tento view vraci TOP 3 trenery s nejvetsim dosahem studentu,
 -- jinymi slovy trenery, kteri vyucuji kurzy s nejvetsim poctem zapsanych studentu
+-- 
+DROP VIEW IF EXISTS fullness;
 
-CREATE VIEW full AS
-SELECT course_id, COUNT(student_id) FROM student_course_signup
-GROUP BY course_id ;
+-- 
+CREATE VIEW
+    fullness AS
+SELECT
+    course_id,
+    COUNT(student_id)
+FROM
+    student_course_signup
+GROUP BY
+    course_id;
 
 -- tady to musim napojit na course tabulku na id_trainer
-
