0f202b60e86ddc29ee72e455380f9c06090265e9
Vigo <miroslav.viktorin@seznam.cz>
2024-11-10 16:59:41 +0100

    data v databazi

--- DDL/Tables/course.sql
+++ DDL/Tables/course.sql
@@ -8,8 +8,8 @@ CREATE TABLE
         start_date TIMESTAMP NOT NULL,
         end_date TIMESTAMP NOT NULL,
         PRIMARY KEY (course_id),
-        FOREIGN KEY (trainer_id)e REFERENCES trainer (trainer_id),
-        CONSTRAINT valid_date CHECK (nd_date > start_date)
+        FOREIGN KEY (trainer_id) REFERENCES trainer (trainer_id),
+        CONSTRAINT valid_date CHECK (end_date > start_date)
     );
 
 -- 
