58115047e9929043d5b6447f2ae367408e165a54
Vigo <miroslav.viktorin@seznam.cz>
2024-11-10 20:05:05 +0100

    final

--- DDL/Tables/trainer.sql
+++ DDL/Tables/trainer.sql
@@ -10,31 +10,3 @@ CREATE TABLE
         PRIMARY KEY (trainer_id)
     );
 
--- INSERT INTO
---     trainer (first_name, last_name, email, expertise)
--- VALUES
---     (
---         'Adam',
---         'Bublina',
---         'adam.bublina@sdacdmy.com',
---         'Python Basics, Machine Learning'
---     ),
---     (
---         'Petr',
---         'Cmoudik',
---         'petr.cmoudik@sdacdemy.com',
---         'HTML,CSS, Python Basics'
---     ),
---     ('Jan', 'Duchacek', 'jan.duchacek@sdacdemy.com', 'GIT'),
---     (
---         'David',
---         'Srdicko',
---         'david.srdicko@sdacdemy.com',
---         'SQL, Database Design'
---     ),
---     (
---         'Robert',
---         'Rumburak',
---         'petr.rumburak@sdacdemy.com',
---         'Python Advanced'
---     );
