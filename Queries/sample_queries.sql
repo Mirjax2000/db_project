dd9c19f09ce84297dfb3aedf049568b5907f7a42
Vigo <miroslav.viktorin@seznam.cz>
2024-11-10 20:02:02 +0100

    final touch

--- Queries/sample_queries.sql
+++ Queries/sample_queries.sql
@@ -1,4 +1,29 @@
 -- zde ulozte par SELECT dotazu vyuzivajicich JOIN, prip. UNION nad tabulkami v projektu
 -- 
+-- priklad 1
+-- 
+-- SELECT
+--     s.first_name,
+--     s.last_name,
+--     a.city,
+--     a.street
+-- FROM
+--     student s
+--     INNER JOIN address a ON a.address_id = s.address_id
+-- GROUP BY
+--     s.first_name,
+--     s.last_name,
+--     a.city,
+--     a.street;
+-- 
+-- priklad 2
+-- 
 SELECT
-   * FROM report;
+    email
+FROM
+    student
+UNION
+SELECT
+    email
+FROM
+    trainer
