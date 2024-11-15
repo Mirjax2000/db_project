8c6053d1dee0a69caa1a931a6a7c0a9eee7dd90b
Vigo <miroslav.viktorin@seznam.cz>
2024-11-15 20:49:38 +0100

    update views

--- DDL/Views/report_all_data.sql
+++ DDL/Views/report_all_data.sql
@@ -3,3 +3,14 @@
 -- tabulek v projektu spojenych pomoci
 -- odpovidajicich JOINu
 DROP VIEW IF EXISTS report;
+CREATE VIEW report AS
+SELECT s.first_name || ' ' || s.last_name AS "Student",
+    STRING_AGG(ti.title_name, ', '),
+    STRING_AGG((t.first_name || ' ' || t.last_name), ', ')
+FROM student_signup ss
+    INNER JOIN course AS c ON c.course_id = ss.course_id
+    INNER JOIN student AS s ON s.student_id = ss.student_id
+    INNER JOIN trainer AS t ON t.trainer_id = c.trainer_id
+    INNER JOIN title AS ti ON c.title_id = ti.title_id
+GROUP BY "Student"
+ORDER BY "Student";
