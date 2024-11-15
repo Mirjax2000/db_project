8561f9cf12bd67404cd68916fd1dedfe670e0398
Vigo <miroslav.viktorin@seznam.cz>
2024-11-15 20:22:55 +0100

    update

--- DDL/Views/trainers_teaching_atleast_2_courses.sql
+++ DDL/Views/trainers_teaching_atleast_2_courses.sql
@@ -1,4 +1,13 @@
 -- tento view vraci trenery kteri vyucuji alespon 2 kurzy
 -- 
-DROP VIEW IF EXISTS skilled;
-
+DROP VIEW IF EXISTS skilled_trainers;
+CREATE VIEW skilled_trainers AS
+SELECT t.first_name || ' ' || t.last_name AS jmeno,
+    COUNT(c.course_id) AS "pocet kurzu",
+    STRING_AGG(ti.title_name, ', ') AS kurzy
+FROM trainer AS t
+    INNER JOIN course AS c ON c.trainer_id = t.trainer_id
+    INNER JOIN title AS ti ON c.title_id = ti.title_id
+GROUP BY jmeno
+HAVING COUNT(c.course_id) >= 2
+ORDER BY "pocet kurzu" DESC
